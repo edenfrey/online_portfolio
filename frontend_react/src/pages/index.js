6b2f9a6016b4897a20aada61a71f8f30ddb81bab
Eden Frey <eden0frey@LittleCrusader.TheCrusade.net>
2023-03-10 17:45:52 +0800

    Back-end Rework | Front-end Setup

---
+++ frontend_react/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import Projects from "./Projects/Projects";
+import Blog from "./Blog/Blog";
+import Contact from "./Contact/Contact";
+
+export{Home, Projects, Blog, Contact}
